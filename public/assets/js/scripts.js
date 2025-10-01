39b0e3a0781c440b0886ddd889228f4d8c48d4ea
SoulFreq <higherselfstudi01@gmail.com>
2025-09-30 22:54:19 -0400

    Delete public directory

--- public/assets/js/scripts.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const fadeElements = document.querySelectorAll('.fade-in');
-  fadeElements.forEach(el => {
-    setTimeout(() => el.classList.add('visible'), 200);
-  });
-});
